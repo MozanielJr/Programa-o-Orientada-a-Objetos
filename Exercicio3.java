ffa8ee82c8162cc7dc50a320669cd60a2b4211b8
MozanielJR <113473875+MozanielJr@users.noreply.github.com>
2023-09-22 20:53:10 -0300

    Exercicio3

---
+++ Exercicio3.java
@@ -0,0 +1,209 @@
+public class Pessoa {
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Pessoa(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
+
+public class Fornecedor extends Pessoa {
+    private double valorCredito;
+    private double valorDivida;
+
+    public Fornecedor(String nome, String endereco, String telefone, double valorCredito, double valorDivida) {
+        super(nome, endereco, telefone);
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+    }
+
+    public double getValorCredito() {
+        return valorCredito;
+    }
+
+    public void setValorCredito(double valorCredito) {
+        this.valorCredito = valorCredito;
+    }
+
+    public double getValorDivida() {
+        return valorDivida;
+    }
+
+    public void setValorDivida(double valorDivida) {
+        this.valorDivida = valorDivida;
+    }
+
+    public double obterSaldo() {
+        return valorCredito - valorDivida;
+    }
+}
+
+public class Empregado extends Pessoa {
+    private int codigoSetor;
+    private double salarioBase;
+    private double imposto;
+
+    public Empregado(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto) {
+        super(nome, endereco, telefone);
+        this.codigoSetor = codigoSetor;
+        this.salarioBase = salarioBase;
+        this.imposto = imposto;
+    }
+
+    public int getCodigoSetor() {
+        return codigoSetor;
+    }
+
+    public void setCodigoSetor(int codigoSetor) {
+        this.codigoSetor = codigoSetor;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public double getImposto() {
+        return imposto;
+    }
+
+    public void setImposto(double imposto) {
+        this.imposto = imposto;
+    }
+
+    public double calcularSalario() {
+        return salarioBase - (salarioBase * (imposto / 100));
+    }
+}
+
+public class Administrador extends Empregado {
+    private double ajudaDeCusto;
+
+    public Administrador(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto, double ajudaDeCusto) {
+        super(nome, endereco, telefone, codigoSetor, salarioBase, imposto);
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
+
+    public double getAjudaDeCusto() {
+        return ajudaDeCusto;
+    }
+
+    public void setAjudaDeCusto(double ajudaDeCusto) {
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() + ajudaDeCusto;
+    }
+}
+
+public class Operario extends Empregado {
+    private double valorProducao;
+    private double comissao;
+
+    public Operario(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto, double valorProducao, double comissao) {
+        super(nome, endereco, telefone, codigoSetor, salarioBase, imposto);
+        this.valorProducao = valorProducao;
+        this.comissao = comissao;
+    }
+
+    public double getValorProducao() {
+        return valorProducao;
+    }
+
+    public void setValorProducao(double valorProducao) {
+        this.valorProducao = valorProducao;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() + (valorProducao * (comissao / 100));
+    }
+}
+
+public class Vendedor extends Empregado {
+    private double valorVendas;
+    private double comissao;
+
+    public Vendedor(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto, double valorVendas, double comissao) {
+        super(nome, endereco, telefone, codigoSetor, salarioBase, imposto);
+        this.valorVendas = valorVendas;
+        this.comissao = comissao;
+    }
+
+    public double getValorVendas() {
+        return valorVendas;
+    }
+
+    public void setValorVendas(double valorVendas) {
+        this.valorVendas = valorVendas;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() + (valorVendas * (comissao / 100));
+    }
+}
+
+public class Teste {
+    public static void main(String[] args) {
+        Fornecedor fornecedor = new Fornecedor("Fornecedor", "Rua A", "123456789", 1000.0, 500.0);
+        System.out.println("Saldo do Fornecedor: " + fornecedor.obterSaldo());
+
+        Administrador administrador = new Administrador("Administrador", "Rua B", "987654321", 1, 2000.0, 10.0, 300.0);
+        System.out.println("Salário do Administrador: " + administrador.calcularSalario());
+
+        Operario operario = new Operario("Operario", "Rua C", "555555555", 2, 1500.0, 5.0, 1000.0, 5.0);
+        System.out.println("Salário do Operario: " + operario.calcularSalario());
+
+        Vendedor vendedor = new Vendedor("Vendedor", "Rua D", "777777777", 3, 1800.0, 8.0, 5000.0, 7.0);
+        System.out.println("Salário do Vendedor: " + vendedor.calcularSalario());
+    }
+}
